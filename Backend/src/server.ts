a55c5de1e2c08e4a0f91913b4350e7a995c7c5e6
MayaraSs <may_stein@hotmail.com>
2020-06-07 20:18:00 -0300

    configuring a connection to the database

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,36 +1,9 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-const users = ["Mayara", "Mayza", "Matheus"];
-
-app.get("/users", (request, response) => {
-  console.log("Listagem de usuários");
-
-  return response.json(users);
-});
-
-app.get("/users/:id", (request, response) => {
-  const id = Number(request.params.id);
-
-  const user = users[id];
-
-  return response.json(user);
-});
-
-app.post("/users", (request, response) => {
-  const data = request.body;
-
-  console.log(data);
-
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-
-  return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
